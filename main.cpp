767aa219548e1a91b10373ecdf8c64d1752a70e1
Alejandro1998elebebecito <49255376+Alejandro1998elebebecito@users.noreply.github.com>
2019-08-26 10:10:40 -0500

    Agregue CPP

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
-
+#include <string.h>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <map>
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    return 0;
+int numeromujeres;
+string country;
+string nombre;
+string appellido;
+
+map<string, int> list_of_conquests;
+
+void contarpaises(int numeromujeres){
+    for (int i = 0; i < numeromujeres; i++) {
+        cin>>country>>nombre>>appellido;
+        list_of_conquests[country] += 1;
+    }
+}
+void imprimir(pair<string, int>  const& a)
+{
+
+    cout << a.first << "   " << a.second << endl;
+}
+
+int main()
+{
+    cin>>numeromujeres;
+    contarpaises(numeromujeres);
+    for_each (
+            list_of_conquests.begin(),
+            list_of_conquests.end(),
+            imprimir
+    );
 }
